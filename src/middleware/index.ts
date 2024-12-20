40e73c58a9b8d7820d809ea3bab0a4059b21a66a
Severin Ibarluzea <seveibar@gmail.com>
2024-12-19 16:09:12 -0800

    Add additional middlewares to exports

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,5 +2,7 @@ export * from "./http-exceptions.js"
 export * from "./with-default-exception-handling.js"
 export * from "./with-logger.js"
 export * from "./with-cors.js"
+export * from "./with-ctx-error.js"
+export * from "./with-request-logging.js"
 
 export { Middleware, MiddlewareChain } from "./types.js"
