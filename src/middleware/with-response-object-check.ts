a883fc8e54acad632dd8ed7927bb9e2ae0d672bb
Arnav K <arnavkaushal09@gmail.com>
2025-03-12 00:46:00 +0530

    feat: throw error when responding with raw json (#32)
    * tune
    
    * add test
    
    * chore
    
    * booooooooooom
    
    * wtf
    
    * test
    
    * rename to withResponseObjectCheck
    
    * Update do-not-allow-raw-json.test.ts

---
+++ src/middleware/with-response-object-check.ts
@@ -0,0 +1,18 @@
+import { ResponseValidationError } from "./http-exceptions.js"
+import { Middleware } from "./types.js"
+import { RouteSpec } from "src/types/route-spec.js"
+
+export const withResponseObjectCheck: Middleware<
+  { routeSpec: RouteSpec<any> },
+  {}
+> = async (req, ctx, next) => {
+  const rawResponse = await next(req, ctx)
+
+  if (typeof rawResponse === "object" && !(rawResponse instanceof Response)) {
+    throw new Error(
+      "Use ctx.json({...}) instead of returning an object directly."
+    )
+  }
+
+  return rawResponse
+}
