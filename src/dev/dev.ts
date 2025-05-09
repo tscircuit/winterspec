c72c6fba5ef99bea1abba82ce06f55e19dd05d3c
seveibar <seveibar@gmail.com>
2025-05-09 16:14:00 -0700

    add export

--- src/dev/dev.ts
+++ src/dev/dev.ts
@@ -2,6 +2,8 @@ import { startDevServer } from "./dev-server.js"
 import { startHeadlessDevBundler } from "./headless/start-bundler.js"
 import { startHeadlessDevServer } from "./headless/start-server.js"
 
+export { startDevServer2 } from "./dev-server2.js"
+
 export const devServer = {
   startDevServer,
   headless: {
