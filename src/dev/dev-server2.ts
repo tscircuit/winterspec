2f12bd5560eba3b206d658e34b45b4b9f6650c4a
Severin Ibarluzea <seve700@gmail.com>
2025-09-25 15:51:03 -0700

    Revert "Fix dev server build concurrency and type resolution (#42)"
    This reverts commit 6c2e2c2a299b36df77219a78399921c58cc9ca97.

--- src/dev/dev-server2.ts
+++ src/dev/dev-server2.ts
@@ -6,7 +6,6 @@ import http from "node:http"
 import path from "node:path"
 import fs from "node:fs/promises"
 import { isGitIgnored } from "globby"
-import { Mutex } from "async-mutex"
 import { getTempPathInApp } from "src/bundle/get-temp-path-in-app.js"
 import { constructManifest } from "src/bundle/construct-manifest.js"
 import { formatMessages } from "esbuild"
@@ -52,8 +51,6 @@ export const startDevServer2 = async (options: StartDevServerOptions) => {
     })
     await fs.writeFile(manifestPath, manifestContent, "utf-8")
   }
-
-  const buildMutex = new Mutex()
 
   const build = async () => {
     options.onBuildStart?.()
@@ -212,12 +209,10 @@ export const startDevServer2 = async (options: StartDevServerOptions) => {
   })
 
   const handleFileChange = async (isManifestChange: boolean = false) => {
-    await buildMutex.runExclusive(async () => {
-      if (isManifestChange) {
-        await updateManifest()
-      }
-      await build()
-    })
+    if (isManifestChange) {
+      await updateManifest()
+    }
+    await build()
   }
 
   watcher.on("change", async (file) => {
@@ -236,7 +231,8 @@ export const startDevServer2 = async (options: StartDevServerOptions) => {
 
   // Initial build is triggered by watcher's ignoreInitial: false
   // If ignoreInitial were true, you'd call:
-  await handleFileChange(true)
+  await updateManifest()
+  await build()
 
   const stop = async () => {
     watcher.close()
